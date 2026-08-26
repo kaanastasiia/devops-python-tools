acaf9ee2e1351828c804292ea72fa250036e44ec
Anastasiia <98601803+kaanastasiia@users.noreply.github.com>
2026-08-27 00:09:16 +0300

    finish search_though_config function

--- config-auditor/config_auditor.py
+++ config-auditor/config_auditor.py
@@ -3,23 +3,49 @@ from pathlib import Path
 
 def config_auditor(conf_path_arg):
     conf_path = Path(conf_path_arg)
+    listen_port_pattern = re.compile(r"(?P<directive>listen)\s+(?:(?P<ip>[\w\.\:\[\]]+):)?(?P<port>\d+)")
+    server_directive_pattern = re.compile(r"^\s*(?P<directive>server)\s+(?P<ip>[\w\.\-\[\]]+)(?::(?P<port>\d+))?")
+    server_name_pattern = re.compile(r"(?P<directive>server_name)\s+(?P<domains>[^;]+);")
+    search_pattern = [listen_port_pattern, server_name_pattern, server_directive_pattern]
+    
     if conf_path.is_file() and conf_path.suffix == ".conf":
         with open(conf_path) as conf:
-            for line in conf:
-                search_through_config(line)
+            for line_num, line in enumerate(conf, start=1):
+                for pattern in search_pattern:
+                    search_through_config(line_num, line, pattern)
 
-    #elif conf_path.is_dir():
-    #    for file in Path(conf_path).rglob("*.conf"):
-    #        with open(file) as log:
+    elif conf_path.is_dir():
+        for config in Path(conf_path).rglob("*.conf"):
+            with open(config) as conf:
+                for line_num, line in enumerate(conf, start=1):
+                    for pattern in search_pattern:
+                        search_through_config(line_num, line, pattern)
 
-def search_through_config(config_line):
-    search_pattern=r"(?P<directive>listen)\s+(?:[\w\.\:\[\]]+\:)?(?P<port>\d+)"
-    match = re.search(search_pattern, config_line)
-    if match:
-        directive = match.group("directive")
-        port = match.group("port")
-        combined = f"{directive} {port}"
-        print (combined)
+def search_through_config(line_num, config_line, pattern):
+    match = re.search(pattern, config_line)
+    if not match:
+        return
+    groups = match.groupdict()
+    directive = groups.get("directive")
+    if directive == "listen":
+        ip = groups.get("ip")
+        port = groups.get("port")
+        ip_str = f"{ip}:" if ip else ""
+        combined = f"{line_num} {directive} {ip_str}{port}"
+        print(combined)
+
+    elif directive == "server_name":
+        domains = groups.get("domains", "").strip()
+        combined = f"{line_num} {directive} {domains}"
+        print(combined)
+
+    elif directive == "server":
+        ip = groups.get("ip")
+        ip_str = f"{ip}:" if ip else ""
+        port = groups.get("port")
+        combined = f"{line_num} {directive} {ip_str}{port}"
+        print(combined)
+
 
 if __name__=="__main__":
     config_file=sys.argv[1]
